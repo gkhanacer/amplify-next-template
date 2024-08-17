eefe586c36b8077435b6b169349cf6be19e97647
gkhanacer <gkhanacer@gmail.com>
2024-08-17 20:22:27 +0200

    auth related parts removed

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+// import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
 
 import { Authenticator } from '@aws-amplify/ui-react'
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
